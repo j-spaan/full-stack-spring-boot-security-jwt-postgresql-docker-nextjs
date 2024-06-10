e01c410f93ba4a3823c9ed7d02fcbe56fec391a0
Jeffrey Spaan <j-spaan@users.noreply.github.com>
2024-06-10 13:51:57 +0200

    Create Global Exception Handling Files

---
+++ backend/src/main/java/com/example/backend/payload/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,66 @@
+package com.example.backend.payload.exception.handler;
+
+import com.example.backend.i18n.I18nService;
+import com.example.backend.payload.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.net.URI;
+
+/**
+ * Global exception handler for handling exceptions across the whole application.
+ * Uses Spring's @ControllerAdvice to centralize exception handling logic.
+ * It handles specific exceptions and provides a unified response structure.
+ *
+ * @author Jeffrey Spaan
+ * @since 2024-06-10
+ */
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+
+    // URI for error is set within the application.yml file
+    @Value("${spring.application.error-uri}")
+    private String errorUri;
+
+    private final I18nService i18nService;
+
+    /**
+     * Handles ResourceNotFoundException and returns a ProblemDetail object
+     * with HTTP status 404 (Not Found) and a localized error message.
+     *
+     * @param ex the ResourceNotFoundException thrown when a resource is not found
+     * @return ProblemDetail object containing the error details and HTTP status
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    protected ProblemDetail handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return getProblemDetail(
+                HttpStatus.NOT_FOUND,
+                i18nService.getMessage(
+                        ex.getMessage(),
+                        ex.getArgs()
+                )
+        );
+    }
+
+    /**
+     * Constructs a ProblemDetail object with the specified HTTP status and detail message.
+     * Sets the type of the problem detail to the configured error URI.
+     *
+     * @param httpStatus the HTTP status to set for the ProblemDetail
+     * @param detail the detail message to set for the ProblemDetail
+     * @return ProblemDetail object with the specified status and detail message
+     */
+    private ProblemDetail getProblemDetail(HttpStatus httpStatus, String detail) {
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(
+                httpStatus,
+                detail
+        );
+        pd.setType(URI.create(errorUri));
+        return pd;
+    }
+}
