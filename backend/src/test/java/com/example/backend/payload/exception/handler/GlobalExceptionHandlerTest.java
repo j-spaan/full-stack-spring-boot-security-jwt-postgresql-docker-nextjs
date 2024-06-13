6786cd8088e8642f9b4d16a2028a3063decf0f02
Jeffrey Spaan <j-spaan@users.noreply.github.com>
2024-06-13 12:45:21 +0200

    Extend GlobalExceptionHandlerTest with Method InvalidCredentialException

--- backend/src/test/java/com/example/backend/payload/exception/handler/GlobalExceptionHandlerTest.java
+++ backend/src/test/java/com/example/backend/payload/exception/handler/GlobalExceptionHandlerTest.java
@@ -1,6 +1,7 @@
 package com.example.backend.payload.exception.handler;
 
 import com.example.backend.i18n.I18nService;
+import com.example.backend.payload.exception.InvalidCredentialsException;
 import com.example.backend.payload.exception.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,9 +17,10 @@ import static org.mockito.Mockito.when;
 class GlobalExceptionHandlerTest {
 
     private static final String ERROR_URI = "https://example.com/error";
-    private static final String ERROR_MESSAGE = "Resource not found";
-    private static final String I18N_MESSAGE = "Resource not found (localized)";
+    private static final String ERROR_MESSAGE = "Test error message";
+    private static final String I18N_MESSAGE = "Test error message (localized)";
     private static final int HTTP_STATUS_NOT_FOUND = 404;
+    private static final int HTTP_STATUS_UNAUTHORIZED = 401;
 
     @Mock
     private I18nService i18nService;
@@ -43,6 +45,18 @@ class GlobalExceptionHandlerTest {
         ProblemDetail pd = globalExceptionHandler.handleResourceNotFoundException(ex);
 
         assertEquals(HTTP_STATUS_NOT_FOUND, pd.getStatus());
+        assertEquals(I18N_MESSAGE, pd.getDetail());
+        assertEquals(ERROR_URI, pd.getType().toString());
+    }
+
+    @Test
+    void testHandleInvalidCredentialsException() {
+        InvalidCredentialsException ex = new InvalidCredentialsException(ERROR_MESSAGE, null);
+        when(i18nService.getMessage(ERROR_MESSAGE, (String) null)).thenReturn(I18N_MESSAGE);
+
+        ProblemDetail pd = globalExceptionHandler.handleInvalidCredentialsException(ex);
+
+        assertEquals(HTTP_STATUS_UNAUTHORIZED, pd.getStatus());
         assertEquals(I18N_MESSAGE, pd.getDetail());
         assertEquals(ERROR_URI, pd.getType().toString());
     }
